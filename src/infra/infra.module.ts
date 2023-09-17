de59c80303d2fc44e404f479580c0becbd392c10
Guilherme Andrade <guilhermegm.andrade@gmail.com>
2023-09-17 17:50:06 -0300

    feat: create connection with mongodb database

--- src/infra/infra.module.ts
+++ src/infra/infra.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import 'dotenv/config'
-import { UserRepositoryMongo } from './mongodb/repositories/user-repository-mongo'
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DATABASE_URL as string)],
-  providers: [UserRepositoryMongo],
-  exports: [UserRepositoryMongo],
+  imports: [
+    MongooseModule.forRoot(process.env.DATABASE_URL as string, {
+      dbName: 'management-api',
+    }),
+  ],
 })
 export class InfraModule {}
--- src/presentation/controllers/user.controller.ts
+++ src/presentation/controllers/user.controller.ts
@@ -1,11 +1,14 @@
+import { User } from '@/src/infra/mongodb/schemas/user-schema-mongo'
 import { Controller, Get } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
 
 @Controller('users')
 export class UserController {
-  constructor() {}
+  constructor(@InjectModel(User.name) private catModel: Model<User>) {}
 
   @Get()
   async getAll() {
-    return 'Hello World'
+    return await this.catModel.find().exec()
   }
 }
