124a4be72133b60b9434f63d25dd6ea854d3ed29
Guilherme Andrade <guilhermegm.andrade@gmail.com>
2023-09-16 22:45:04 -0300

    test: ensure create user

---
+++ src/domain/use-cases/register-user-use-case.spec.ts
@@ -0,0 +1,43 @@
+import { InMemoryUserRepository } from '@/test/in-memory/in-memory-user-repository'
+import { RegisterUserUseCase } from './register-user-use-case'
+import { makeUser } from '@/test/factories/make-user'
+
+let inMemoryUserRepository: InMemoryUserRepository
+let sut: RegisterUserUseCase
+
+describe('Register User Use Case', () => {
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository()
+    sut = new RegisterUserUseCase(inMemoryUserRepository)
+  })
+
+  it('should be register a new user', async () => {
+    const user = await makeUser()
+
+    await sut.execute({
+      email: user.email,
+      name: user.name,
+      password: '123456',
+      role: user.role,
+    })
+
+    expect(inMemoryUserRepository.users.length).toBe(1)
+  })
+
+  it('should not be register a new user with same email', async () => {
+    const user = await makeUser({
+      email: 'teste@teste.com',
+    })
+
+    await inMemoryUserRepository.create(user)
+
+    await expect(() =>
+      sut.execute({
+        email: user.email,
+        name: 'john doe',
+        password: '123456',
+        role: 'admin',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
